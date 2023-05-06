81e9d7886e9a69cd83ce99962fd9d9ebce1e1cd4
Sitan Doucara <doucarasitan@gmail.com>
2023-05-06 18:42:02 +0200

    add page description

--- js/carousel.js
+++ js/carousel.js
@@ -1,59 +1,15 @@
-//Slider carousel
-import { getMovies } from "./tmdb.js";
-const movies = await getMovies();
-
-console.log("carousel.js > movies >", movies);
-/** @type {HTMLElement}*/
-const sliderContainers = document.querySelectorAll(".slider");
-const nxtBtn = document.querySelectorAll(".nxt-btn");
-const preBtn = document.querySelectorAll(".pre-btn");
-
-/**
- * Description
- * @param {{title:string, poster_path: string}} movie
- * @returns {string}
- */
-
 //Other
 const carousel = document.querySelector(".carousel");
 const nextBtn = document.querySelector(".next");
 const previousBtn = document.querySelector(".previous");
-//const carouselItems = document.querySelectorAll(".carousel > article");
 
 //const carouselLength = carouselItems.length;
 
-/*const getSlide = (movie) => ` <article>
-<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" 
-      alt = Affiche du film ${movie.title}/>
-<h2>${movie.title}</h2>
-</article>`;*/
-
-//apps.120.design
-const getSlide = (movie) => ` <div class= "api_img">
-<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" 
-      sizes=(min-width: 750px)  alt = Affiche du film ${movie.title}/>
-</div>`;
-
-movies.forEach((movie) => {
-  carousel.innerHTML += getSlide(movie);
-});
-sliderContainers.forEach((item, i) => {
-  let containerDimensions = item.getBoundingClientRect();
-  let containerWidth = containerDimensions.width;
-
-  nxtBtn[i].addEventListener("click", () => {
-    item.scrollLeft += containerWidth;
-  });
-
-  preBtn[i].addEventListener("click", () => {
-    item.scrollLeft -= containerWidth;
-  });
-});
-
 let index = 0;
+const maxIndex = 2;
 
 const moveCarousel = () => {
-  carousel.style.transform = `translate(-${100 * index}vw)`;
+  carousel.style.transform = `translateX(-${index * 100}%)`;
 };
 
 const setControls = () => {
@@ -76,7 +32,7 @@ nextBtn.addEventListener("click", () => {
 });
 
 previousBtn.addEventListener("click", () => {
-  if (index < movies.length - 1) index++;
+  if (index < maxIndex) index++;
   console.log(index);
   moveCarousel();
   setControls();
